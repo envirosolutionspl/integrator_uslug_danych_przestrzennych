b203001dd2e5fb4bd1553d2caf7bffd6ef7b9b8c
MKolasa01 <108238370+MKolasa01@users.noreply.github.com>
2024-09-06 11:09:40 +0200

    Reapply "Amortyzacja kosztów czasowych"
    This reverts commit c57f307c2d370058a5c28a98b330773e4c29b9b3.

--- devision_api.py
+++ devision_api.py
@@ -6,59 +6,45 @@ def get_wojewodztwa():
 
     if response.status_code == 200:
         wojewodztwa = response.json()
-        return wojewodztwa
+        devision_dict = {}
+        for wojewodztwo in wojewodztwa.get('jednAdms', []):
+            wojewodztwo_info = wojewodztwo.get('jednAdm')
+            wojewodztwo_id = wojewodztwo_info.get('wojIIPId')
+            wojewodztwo_nazwa = wojewodztwo_info.get('wojNazwa')
+            devision_dict[wojewodztwo_nazwa] = wojewodztwo_id
+        return devision_dict
     else:
         print(f"Błąd: {response.status_code}")
-        return None
+        
 
 
 def get_powiaty(woj_id):
     url = f"http://mapy.geoportal.gov.pl/wss/service/SLN/guest/sln/pow/PL.PZGIK.200/{woj_id}/skr.json"
     response = requests.get(url)
-    
+ 
     if response.status_code == 200:
         powiaty = response.json()
-        return powiaty
+        powiaty_dict = {}
+        for powiat in powiaty.get('jednAdms'):
+            powiat_info = powiat.get('jednAdm')
+            powiat_id = powiat_info.get('powIIPId')
+            powiat_nazwa = powiat_info.get('powNazwa')
+            powiaty_dict[powiat_nazwa] = powiat_id
+        return powiaty_dict
     else:
         print(f"Błąd: {response.status_code}")
-        return None
+        
     
 def get_gminy(pow_id):
     url = f"http://mapy.geoportal.gov.pl/wss/service/SLN/guest/sln/gmi/PL.PZGIK.200/{pow_id}/pel.json"
     response = requests.get(url)
-    
     if response.status_code == 200:
         gminy = response.json()
-        return gminy
+        gminy_arr = []
+        for gmina in gminy['jednAdms']:
+            gmina_nazwa = gmina['jednAdm'].get('gmNazwa')
+            gminy_arr.append(gmina_nazwa)
+        return sorted(gminy_arr)
     else:
         print(f"Błąd: {response.status_code}")
-        return None
-
-def make_administrative_devision_dict():
-    devision_dict = {}
-
-    for wojewodztwo in get_wojewodztwa().get('jednAdms', []):
-        wojewodztwo_info = wojewodztwo.get('jednAdm')
-        wojewodztwo_id = wojewodztwo_info.get('wojIIPId')
-        wojewodztwo_nazwa = wojewodztwo_info.get('wojNazwa')
-        devision_dict[wojewodztwo_nazwa] = {}
-        print(wojewodztwo_nazwa)
         
-        for powiat in get_powiaty(wojewodztwo_id).get('jednAdms'):
-            powiat_info = powiat.get('jednAdm')
-            powiat_id = powiat_info.get('powIIPId')
-            powiat_nazwa = powiat_info.get('powNazwa')
-            devision_dict[wojewodztwo_nazwa][powiat_nazwa] = []
-            #print(powiat_nazwa)
-
-            for gmina in get_gminy(powiat_id).get('jednAdms'):
-                devision_dict[wojewodztwo_nazwa][powiat_nazwa].append(gmina.get('jednAdm').get('gmNazwa'))
-                #print(gmina.get('jednAdm').get('gmNazwa'))
-
-    print('\n\n\n') 
-    print(devision_dict)
-
-
-#print(get_gminy('1846e6d9-3e9c-4328-a203-d6d4ccd61aa2'))
-
-make_administrative_devision_dict()
--- web_service_plugin_dialog.py
+++ web_service_plugin_dialog.py
@@ -24,13 +24,18 @@
 
 import os
 import re
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from qgis.PyQt import uic
 from qgis.PyQt import QtWidgets
+from devision_api import get_gminy, get_powiaty, get_wojewodztwa
 
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'web_service_plugin_dialog_base.ui'))
+
 
 
 class WebServicePluginDialog(QtWidgets.QDialog, FORM_CLASS):
@@ -50,31 +55,20 @@ class WebServicePluginDialog(QtWidgets.QDialog, FORM_CLASS):
 
 
 
-        self.load_administrative_division('gminy.txt')   
+        self.load_wojewodztwa()
         self.comboBox_gminy.currentIndexChanged.connect(self.on_combobox_gminy_changed)
         self.comboBox_powiaty.currentIndexChanged.connect(self.on_combobox_powiaty_changed)
         self.comboBox_wojewodztwa.currentIndexChanged.connect(self.on_combobox_wojewodztwa_changed)
 
 
-    def load_administrative_division(self, path): 
-        self.devision_dict = {}
-        
-        with open(path, 'r', encoding='utf-8') as infile:
-            next(infile)
-            for line in infile:
-                info = line.split('\t')
-
-                if info[0] not in self.devision_dict:
-                    self.devision_dict[info[0]] = {info[1] : [info[2]]}
-                elif info[1] not in self.devision_dict[info[0] ]:
-                    self.devision_dict[info[0]][info[1]] = [info[2]]
-                else: 
-                    self.devision_dict[info[0]][info[1]].append(info[2])
-        
+    def load_wojewodztwa(self): 
+        self.devision_dict = get_wojewodztwa()
         self.comboBox_wojewodztwa.addItems(['województwo'] + sorted(self.devision_dict.keys()))
         self.comboBox_powiaty.addItems(['powiat'])
         self.comboBox_gminy.addItems(['gmina'])
-        
+        self.comboBox_wojewodztwa.setCurrentIndex(0)
+        self.comboBox_powiaty.setCurrentIndex(0)
+        self.comboBox_gminy.setCurrentIndex(0)
 
     def on_combobox_gminy_changed(self):
         selected_option = self.comboBox_gminy.currentText()
@@ -82,12 +76,17 @@ class WebServicePluginDialog(QtWidgets.QDialog, FORM_CLASS):
 
     def on_combobox_powiaty_changed(self):
         selected_option = self.comboBox_powiaty.currentText()
-        if selected_option == 'powiat':
+        
+        if selected_option == 'powiat' or not selected_option:
             self.comboBox_gminy.clear()
             self.comboBox_gminy.addItems(['gmina'])
         else:
             wojewodztwo = self.comboBox_wojewodztwa.currentText()
-            gminy = sorted(self.devision_dict[wojewodztwo][selected_option])
+            if isinstance(self.devision_dict[wojewodztwo][selected_option], str):
+                powiat_id = self.devision_dict[wojewodztwo][selected_option]
+                self.devision_dict[wojewodztwo][selected_option] = get_gminy(powiat_id)
+                print(get_gminy(powiat_id))
+            gminy = self.devision_dict[wojewodztwo][selected_option]
             self.comboBox_gminy.clear()
             self.comboBox_gminy.addItems(['gmina'] + gminy)
 
@@ -98,6 +97,9 @@ class WebServicePluginDialog(QtWidgets.QDialog, FORM_CLASS):
             self.comboBox_powiaty.clear()
             self.comboBox_powiaty.addItems(['powiat'])
         else:
+            if isinstance(self.devision_dict[selected_option], str):
+                wojewodztwo_id = self.devision_dict[selected_option]
+                self.devision_dict[selected_option] = get_powiaty(wojewodztwo_id)
             powiaty = sorted(self.devision_dict[selected_option].keys())
             self.comboBox_powiaty.clear()
             self.comboBox_powiaty.addItems(['powiat'] + powiaty)
