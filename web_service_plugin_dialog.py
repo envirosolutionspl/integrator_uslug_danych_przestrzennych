c57f307c2d370058a5c28a98b330773e4c29b9b3
MKolasa01 <108238370+MKolasa01@users.noreply.github.com>
2024-09-06 11:06:32 +0200

    Revert "Amortyzacja kosztów czasowych"
    This reverts commit 4873dc54517358ab85cf0dd6a8e19367a813d115.

--- web_service_plugin_dialog.py
+++ web_service_plugin_dialog.py
@@ -24,18 +24,13 @@
 
 import os
 import re
-import sys
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from qgis.PyQt import uic
 from qgis.PyQt import QtWidgets
-from devision_api import get_gminy, get_powiaty, get_wojewodztwa
 
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'web_service_plugin_dialog_base.ui'))
-
 
 
 class WebServicePluginDialog(QtWidgets.QDialog, FORM_CLASS):
@@ -55,20 +50,31 @@ class WebServicePluginDialog(QtWidgets.QDialog, FORM_CLASS):
 
 
 
-        self.load_wojewodztwa()
+        self.load_administrative_division('gminy.txt')   
         self.comboBox_gminy.currentIndexChanged.connect(self.on_combobox_gminy_changed)
         self.comboBox_powiaty.currentIndexChanged.connect(self.on_combobox_powiaty_changed)
         self.comboBox_wojewodztwa.currentIndexChanged.connect(self.on_combobox_wojewodztwa_changed)
 
 
-    def load_wojewodztwa(self): 
-        self.devision_dict = get_wojewodztwa()
+    def load_administrative_division(self, path): 
+        self.devision_dict = {}
+        
+        with open(path, 'r', encoding='utf-8') as infile:
+            next(infile)
+            for line in infile:
+                info = line.split('\t')
+
+                if info[0] not in self.devision_dict:
+                    self.devision_dict[info[0]] = {info[1] : [info[2]]}
+                elif info[1] not in self.devision_dict[info[0] ]:
+                    self.devision_dict[info[0]][info[1]] = [info[2]]
+                else: 
+                    self.devision_dict[info[0]][info[1]].append(info[2])
+        
         self.comboBox_wojewodztwa.addItems(['województwo'] + sorted(self.devision_dict.keys()))
         self.comboBox_powiaty.addItems(['powiat'])
         self.comboBox_gminy.addItems(['gmina'])
-        self.comboBox_wojewodztwa.setCurrentIndex(0)
-        self.comboBox_powiaty.setCurrentIndex(0)
-        self.comboBox_gminy.setCurrentIndex(0)
+        
 
     def on_combobox_gminy_changed(self):
         selected_option = self.comboBox_gminy.currentText()
@@ -76,17 +82,12 @@ class WebServicePluginDialog(QtWidgets.QDialog, FORM_CLASS):
 
     def on_combobox_powiaty_changed(self):
         selected_option = self.comboBox_powiaty.currentText()
-        
-        if selected_option == 'powiat' or not selected_option:
+        if selected_option == 'powiat':
             self.comboBox_gminy.clear()
             self.comboBox_gminy.addItems(['gmina'])
         else:
             wojewodztwo = self.comboBox_wojewodztwa.currentText()
-            if isinstance(self.devision_dict[wojewodztwo][selected_option], str):
-                powiat_id = self.devision_dict[wojewodztwo][selected_option]
-                self.devision_dict[wojewodztwo][selected_option] = get_gminy(powiat_id)
-                print(get_gminy(powiat_id))
-            gminy = self.devision_dict[wojewodztwo][selected_option]
+            gminy = sorted(self.devision_dict[wojewodztwo][selected_option])
             self.comboBox_gminy.clear()
             self.comboBox_gminy.addItems(['gmina'] + gminy)
 
@@ -97,9 +98,6 @@ class WebServicePluginDialog(QtWidgets.QDialog, FORM_CLASS):
             self.comboBox_powiaty.clear()
             self.comboBox_powiaty.addItems(['powiat'])
         else:
-            if isinstance(self.devision_dict[selected_option], str):
-                wojewodztwo_id = self.devision_dict[selected_option]
-                self.devision_dict[selected_option] = get_powiaty(wojewodztwo_id)
             powiaty = sorted(self.devision_dict[selected_option].keys())
             self.comboBox_powiaty.clear()
             self.comboBox_powiaty.addItems(['powiat'] + powiaty)
