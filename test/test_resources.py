08c1f3c712a3f9a4f01587656daf709f433df8d8
aglu <alek.glu@gmail.com>
2024-11-18 12:50:49 +0100

    podmiana ikony wtyczki

--- test/test_resources.py
+++ test/test_resources.py
@@ -31,7 +31,7 @@ class WebServicePluginDialogTest(unittest.TestCase):
 
     def test_icon_png(self):
         """Test we can click OK."""
-        path = ':/plugins/WebServicePlugin/icon.png'
+        path = ':/plugins/WebServicePlugin/icon.svg'
         icon = QIcon(path)
         self.assertFalse(icon.isNull())
 
