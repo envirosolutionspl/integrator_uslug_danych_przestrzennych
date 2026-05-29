ccdd0a287fe9ffe8a94cf5faf8a68031536ca002
darek-r <117411844+darek-r@users.noreply.github.com>
2026-05-29 14:12:48 +0200

    zbiorcza aktualizacja
    - refactoring kodu
    - dodanie okna dialogowego progresu
    - poprawa działania okna dialogowego
    - dodanie fallback

--- constants.py
+++ constants.py
@@ -46,3 +46,91 @@ SERVICES_NAMESPACES = {
         'ows': 'http://www.opengis.net/ows/1.1'
     },
 }
+# =============================
+# Parametry do klasy VersionUtils
+# wersja Qt6
+QT_VER = {
+    6: "6."
+}
+
+# =============================
+# Parametry do klasy NetworkUtils
+# Nagłówek w requests
+USER_AGENT_HEADER = "Integrator-QGIS-Client/1.0"
+CONNECTION_HEADER = "close"
+
+TIMEOUT_MS = 5000
+MAX_ATTEMPTS = 3
+CANCEL_CHECK_MS = 500
+HTTP_ERROR_THRESHOLD = 400
+
+# Nazwy atrybutów
+NETWORK_ATTRS = {
+    'HTTP_STATUS': 'HttpStatusCodeAttribute',
+    'HTTP_REASON': 'HttpReasonPhraseAttribute',
+    'REDIRECT': 'RedirectPolicyAttribute',
+    'TIMEOUT': 'TimeoutAttribute'
+}
+
+# RedirectPolicy
+REDIRECT_POLICY_NAME = 'RedirectPolicy'
+REDIRECT_POLICY_NO_LESS_SAFE = 'NoLessSafeRedirectPolicy'
+DEFAULT_REDIRECT_POLICY = 1
+
+# Wartości błędów i statusów
+ERR_TIMEOUT = 'TimeoutError'
+ERR_NONE = 'NoError'
+ERR_CANCELED = 'OperationCanceledError'
+STATUS_SUCCESS = 'brak_bledow'
+STATUS_CANCELED = 'anulowano'
+
+# Komunikaty sieciowe
+MSG_DOWNLOAD_CANCELED = "Pobieranie zostało anulowane."
+MSG_NETWORK_ERROR = "Błąd sieciowy ({}) dla: {}"
+MSG_HTTP_ERROR = "Błąd HTTP {}: {}"
+MSG_EMPTY_CONTENT = "Serwer zwrócił pustą zawartość dla: {}"
+MSG_TIMEOUT = "Przekroczono czas oczekiwania dla: {}"
+MSG_FILE_WRITE_ERROR = "Błąd zapisu do pliku: {}"
+MSG_JSON_DECODE_ERROR = "Błąd JSON: {}"
+MSG_NO_CONNECTION = "Brak połączenia z internetem."
+
+
+# =============================
+# Parametry do klasy StandaloneUrlsFetcher
+SERVICE_TYPES = (
+    "wms",
+    "wmts",
+    "wfs",
+    "wcs",
+)
+
+TERYT_PL = 'PL'
+
+# =============================
+# Parametry do klasy GeoportalServicesFetcher
+#GEOPORTAL endpoints
+GEOPORTAL_WMS_WMTS_URL = (
+    "https://www.geoportal.gov.pl/pl/usluga/uslugi-przegladania-wms-i-wmts/"
+)
+GEOPORTAL_WFS_URL = "https://www.geoportal.gov.pl/pl/usluga/uslugi-pobierania-wfs/"
+GEOPORTAL_WCS_URL = "https://www.geoportal.gov.pl/pl/usluga/uslugi-pobierania-wcs/"
+
+# =============================
+# Parametry do klasy
+# EZIUDP endpoint
+EZIUDP_BASE_URL = "https://integracja.gugik.gov.pl/eziudp/index.php"
+
+# EZIUDP HTML parsing
+XPATH_TABLE_SORTABLE = '//table[contains(@class, "table sortable")]'
+XPATH_TABLE_ROWS = ".//tr[position()>1]"
+XPATH_ROW_COLUMNS = ".//td"
+XPATH_LINK_TAG = ".//a"
+MIN_COLUMNS_FOR_ROW = 6
+DATASET_NAME_COLUMN = 2
+WMS_WMTS_COLUMN = 5
+WFS_WCS_COLUMN = 6
+
+# EZIUDP fetcher settings
+SERVICES_REQUEST_TIMEOUT_SECONDS = 10
+EZIUDP_WMS_WMTS_NEEDLES = ("wms", "wmts")
+EZIUDP_WFS_WCS_NEEDLES = ("wfs", "wcs")
--- integrator_uslug_danych_przestrzennych_dialog.py
+++ integrator_uslug_danych_przestrzennych_dialog.py
@@ -1,4 +1,18 @@
 ﻿# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ * Integrator Uslug Danych Przestrzennych                                  *
+ *                                 A QGIS plugin                           *
+ * Wtyczka umożliwia prezentację danych z serwisów WMS, WMTS, WFS i WCS    *
+ * w postaci warstw w QGIS. Wtyczka wykorzystuje dane z Ewidencji Zbiorów  *
+ * i Usług oraz strony geoportal.gov.pl                                    *
+ * ----------------------------------------------------------------------- *
+ *       begin                : 2026-05-28                                 *
+ *       copyright            : (C) 2026 by EnviroSolutions Sp. z o.o.     *
+ *       email                : office@envirosolutions.pl                  *
+ *       git sha              : $Format:%H$                                *
+ ***************************************************************************/
+"""
 import os
 import sys
 from typing import Dict, List
@@ -7,67 +21,63 @@ from qgis.PyQt import QtWidgets
 from qgis.PyQt import uic
 from qgis.PyQt.QtCore import QSortFilterProxyModel, Qt
 from qgis.PyQt.QtGui import QShowEvent, QStandardItem, QStandardItemModel
-
-from .api.country_urls_fetcher import CountryUrlsFetcher
+from .modules.content_manager import ContentManager
 from .constants import RADIOBUTTONS_SERVICES
-from .utils import QtCompat
+from .utils import QtCompat, SingleTaskManager
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'integrator_uslug_danych_przestrzennych_dialog_base.ui'))
 
-
 class IntegratorUslugPrzestrzennychDialog(QtWidgets.QDialog, FORM_CLASS):
+
     def __init__(self, parent=None):
         super(IntegratorUslugPrzestrzennychDialog, self).__init__(parent)
         self.setupUi(self)
         self.qt_compat = QtCompat()
-        self.country_urls_fetcher = CountryUrlsFetcher()
-        self.country_services_cache: List[Dict[str, str]] = []
-        self.setupDialog()
+        
+        self.content_manager = ContentManager(self)
+        self.content_manager.setTableRefreshFunction(self.setupTable)
+        self.serv_rows = []
+        self.table_setup_task = SingleTaskManager(self._fetchServices, self._finishTableSetup)
+        self.plugin_name = ''
+
         self.setupSignals()
-        self.setupTable()
 
-    def setupDialog(self) -> None:
-        self.img_main.setMargin(9)
-
-    def setupSignals(self) -> None:
-        for obj in RADIOBUTTONS_SERVICES:
-            widget_obj = getattr(self, obj)
-            widget_obj.toggled.connect(self.setupTable)
-        self.search_lineedit.textChanged.connect(self.applySearchFilter)
-
-    def setupTable(self) -> None:
+        # Inicjacja tabeli 
         self.model = QStandardItemModel()
-        self.model.setHorizontalHeaderLabels(['Nazwa uslugi', 'Adres uslugi'])
-        self.fillServicesTable()
         self.configureTableHeader()
         self.setupSearch()
-        self.applySearchFilter(self.search_lineedit.text())
+        self.setupTable()
 
     def configureTableHeader(self) -> None:
-        resize_interactive = self.qt_compat.getEnum(QtWidgets.QHeaderView, 'ResizeMode', 'Interactive')
-        header = self.services_table.horizontalHeader()
-        header.setSectionResizeMode(0, resize_interactive)
-        self.services_table.setColumnWidth(0, 400)
-        header.setSectionResizeMode(1, resize_interactive)
-        self.services_table.setColumnWidth(1, 500)
-        ascending = self.qt_compat.getEnum(Qt, 'SortOrder', 'AscendingOrder')
-        self.services_table.horizontalHeader().setSortIndicator(0, ascending)
-        self.services_table.setSortingEnabled(True)
-        header = self.services_table.verticalHeader()
-        align_center = self.qt_compat.getEnum(Qt, 'AlignmentFlag', 'AlignCenter')
-        header.setDefaultAlignment(align_center)
+        " Wstępna konfiguracja tabeli "
 
-    def fillServicesTable(self) -> None:
-        for service_row in self.getServicesRows():
-            row = [
-                QStandardItem(service_row['dataset_name']),
-                QStandardItem(service_row['url']),
-            ]
-            self.model.appendRow(row)
+        # QtCompat
+        resize_interactive = self.qt_compat.getEnum(QtWidgets.QHeaderView, 'ResizeMode', 'Interactive')
+        ascending = self.qt_compat.getEnum(Qt, 'SortOrder', 'AscendingOrder')
+        align_center = self.qt_compat.getEnum(Qt, 'AlignmentFlag', 'AlignCenter')
+
+        # Inicjacja modelu tabeli
+        self.model = QStandardItemModel()
+        self.model.setHorizontalHeaderLabels(['', ''])
         self.services_table.setModel(self.model)
+
+        # Ustawienia poziome
+        h_header = self.services_table.horizontalHeader()
+        h_header.setSectionResizeMode(0, resize_interactive)
+        h_header.setSectionResizeMode(1, resize_interactive)
+        h_header.setSortIndicator(0, ascending)
+
+        # Ustawienia pionowe
+        v_header = self.services_table.verticalHeader()
+        v_header.setDefaultSectionSize(14)
+        v_header.setDefaultAlignment(align_center)
+
+        self.services_table.setColumnWidth(0, 400)
+        self.services_table.setColumnWidth(1, 500)
+        self.services_table.setSortingEnabled(True)
 
     def setupSearch(self) -> None:
         self.proxy_model = QSortFilterProxyModel()
@@ -75,10 +85,106 @@ class IntegratorUslugPrzestrzennychDialog(QtWidgets.QDialog, FORM_CLASS):
         self.proxy_model.setFilterKeyColumn(0)
         self.services_table.setModel(self.proxy_model)
 
+    def setupDialog(self, plugin_name, plugin_version) -> None:
+        self.img_main.setMargin(9)
+        self.setWindowTitle('%s %s' % (plugin_name, plugin_version))
+        self.lbl_pluginVersion.setText('%s %s' % (plugin_name, plugin_version))
+        self.plugin_name = plugin_name
+
+    # =============================
+    # Fukcje obsługujące sygnały
+
+    def setupSignals(self) -> None:
+        for obj in RADIOBUTTONS_SERVICES:
+            widget_obj = getattr(self, obj)
+            widget_obj.toggled.connect(self.setupTable)
+        self.search_lineedit.textChanged.connect(self.applySearchFilter)
+        self.add_btn.clicked.connect(self.addService)
+
+    def setupTable(self) -> None:
+        if self.content_manager.isStandaloneFetchTaskRunning() and not self.content_manager.isStandaloneServicesCacheReady():
+            self.pushMessageOverTable(" Aktualizacja usług...","Pobieranie danych.")
+            return
+        if self.table_setup_task.isRunning():
+            return
+        self.setEnabledRadiobuttons(False)
+        self.pushMessageOverTable(" Aktualizacja usług...","Pobieranie")
+        if not self.table_setup_task.run(): # SingleTaskManager(self.fetchServices, self.finishTableSetup)
+            self.pushMessageOverTable(" Aktualizacja usług...","Nieoczekiwany błąd.")
+
+    def _fetchServices(self) -> None:
+        self.serv_rows = self.getServicesRows()
+
+    def _finishTableSetup(self) -> None:
+        self.fillServicesTable(self.serv_rows)
+        self.applySearchFilter(self.search_lineedit.text())
+        self.setEnabledRadiobuttons(True)
+
     def applySearchFilter(self, text: str) -> None:
         case_insensitive = self.qt_compat.getEnum(Qt, 'CaseSensitivity', 'CaseInsensitive')
         self.proxy_model.setFilterCaseSensitivity(case_insensitive)
         self.proxy_model.setFilterFixedString(text)
+
+    def addService(self):
+
+        # Blokowanie elementów okna
+        self.setEnabledRadiobuttons(False)
+        self.setEnabledTable(False)
+        
+        # Pobranie danych z tabeli i dodanie usług do mapy
+        proxy_model = self.services_table.model()
+        selected_indexes = self.services_table.selectionModel().selectedRows()
+        selected_service_type = self.getSelectedServiceType()
+        self.content_manager.addServiceFromSelection(proxy_model, selected_indexes, selected_service_type)
+
+        # Odblokowanie elementów okna 
+        self.setEnabledRadiobuttons(True)
+        self.setEnabledTable(True)
+   
+    # =============================
+    # Fukcje obsługujące elementy okna
+
+    def fillServicesTable(self, service_rows: List = []) -> None:
+
+        # Oczyszczenie tablicy
+        row_count = self.model.rowCount()
+        if row_count > 0:
+            self.model.removeRows(0, row_count)
+
+        # Wypełnienie tablicy
+        self.model.setHorizontalHeaderLabels(['Nazwa usługi', 'Adres usługi'])
+        for service_row in service_rows:
+            row = [
+                QStandardItem(service_row['dataset_name']),
+                QStandardItem(service_row['url']),
+            ]
+            self.model.appendRow(row)
+        ascending = self.qt_compat.getEnum(Qt, 'SortOrder', 'AscendingOrder')
+        self.model.sort(0, ascending)
+
+    def pushMessageOverTable(self, message : str, status : str = '') -> None:
+        " Czyści tabelę i wykorzysuje pierwsze pole jako miejsce na komunikat i opcjonalnie status "
+
+        # Oczyszczenie tablicy
+        row_count = self.model.rowCount()
+        if row_count > 0:
+            self.model.removeRows(0, row_count)
+
+        # Wstawienie komunikatu
+        self.model.setHorizontalHeaderLabels(['Komunikat', 'Status'])
+        row = [
+                QStandardItem(message),
+                QStandardItem(status)
+            ]
+        self.model.appendRow(row)
+
+    def setEnabledRadiobuttons(self, is_enabled = True):
+        for obj in RADIOBUTTONS_SERVICES:
+            widget_obj = getattr(self, obj)
+            widget_obj.setEnabled(is_enabled)
+
+    def setEnabledTable(self, is_enabled = True):
+        self.services_table.setEnabled(is_enabled)
 
     def getSelectedServiceType(self) -> str:
         if self.wmts_rdbtn.isChecked():
@@ -90,25 +196,17 @@ class IntegratorUslugPrzestrzennychDialog(QtWidgets.QDialog, FORM_CLASS):
         return 'WMS'
 
     def getServicesRows(self) -> List[Dict[str, str]]:
-        return self.country_urls_fetcher.getCountryUrlsByServiceType(
-            self.country_services_cache,
-            self.getSelectedServiceType(),
-        )
-
-    def getSelectedServicesUrls(self) -> Dict[str, str]:
-        model = self.services_table.model()
-        selected_indexes = self.services_table.selectionModel().selectedRows()
-        values = {}
-        for index in selected_indexes:
-            name_index = model.index(index.row(), 0)
-            value_index = model.index(index.row(), 1)
-            values[model.data(name_index)] = model.data(value_index)
-        return values
+        return self.content_manager.getCountryUrlsByServiceType(self.getSelectedServiceType())
+    
+    # =============================
+    # Deklaracja funkcji dziedziczonych
 
     def showEvent(self, event: QShowEvent) -> None:
         super().showEvent(event)
-        self.country_services_cache = self.country_urls_fetcher.fetchCountryUrls()
+        self.content_manager.servicesCacheInit()
         self.setupTable()
+        self.setEnabledRadiobuttons(True)
+        self.setEnabledTable(True)
         self.wms_rdbtn.setFocus()
 
     def closeEvent(self, event: QShowEvent) -> None:
